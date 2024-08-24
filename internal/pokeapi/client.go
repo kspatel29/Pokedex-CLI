07b499daef4de2595ccbcff0c8ccf5a9ab471f41
happyabuse47 <kushpatel2627@gmail.com>
2024-08-24 11:05:28 -0600

    added more commands

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,16 +3,20 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	pokecache "github.com/kspatel29/pokedex/internal/poke_cache"
 )
 
 // Client -
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
